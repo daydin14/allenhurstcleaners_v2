4515669d8ef06ea81d582536030a37d54f110db2
David Aydin <david.aydin6@gmail.com>
2024-09-11 14:26:14 -0400

    Refactored Header with collapsible Sidebar

--- optio-blue/src/components/Header.js
+++ optio-blue/src/components/Header.js
@@ -1,16 +1,32 @@
 // Dependencies
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
+    const closeSidebar = () => {
+        setIsSidebarOpen(false);
+    };
+
     return (
         <header className="header">
-            <nav>
+            <button className="hamburger" onClick={toggleSidebar}>
+                &#9776;
+            </button>
+            <nav className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
+                <button className="close-btn" onClick={closeSidebar}>
+                    &times;
+                </button>
                 <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/services">Services</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
+                    <li><Link to="/" onClick={closeSidebar}>Home</Link></li>
+                    <li><Link to="/about" onClick={closeSidebar}>About</Link></li>
+                    <li><Link to="/services" onClick={closeSidebar}>Services</Link></li>
+                    <li><Link to="/contact" onClick={closeSidebar}>Contact</Link></li>
                 </ul>
             </nav>
         </header>
