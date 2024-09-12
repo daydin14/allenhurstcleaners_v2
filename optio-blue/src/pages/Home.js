437efd4f3a4fd5d87e36dea514aa10502c6fae97
David Aydin <david.aydin6@gmail.com>
2024-09-11 20:43:17 -0400

    Refactor index.js and add Google Tag loading utility, update ignore .env(s)

--- optio-blue/src/pages/Home.js
+++ optio-blue/src/pages/Home.js
@@ -6,6 +6,13 @@ const Home = () => {
         <div>
             <h1>Home Page</h1>
             <p>Welcome to our local business website!</p>
+            <iframe
+                id="google-maps"
+                title="Allenhurst Cleaners"
+                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAEzsheyINm_Nq00YrzuUAXEL_6VkfSpS8&q=Allenhurst+Cleaners"
+                frameborder=""
+                allowfullscreen
+            ></iframe>
         </div>
     );
 };
---
+++ optio-blue/src/utils/Gtag.js
@@ -0,0 +1,20 @@
+export const loadGoogleTag = () => {
+    const googleTagId = process.env.REACT_APP_GOOGLE_TAG_ID;
+    if (!googleTagId) {
+        console.error('Google Tag ID is not defined in the environment variables');
+        return;
+    }
+
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${googleTagId}`;
+    document.head.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+        window.dataLayer.push(arguments);
+    }
+    window.gtag = gtag;
+    gtag('js', new Date());
+    gtag('config', googleTagId);
+};
