26eeb130dba47f08681f3303168fa858d1f98ad5
David Aydin <david.aydin6@gmail.com>
2024-09-11 21:48:44 -0400

    Refactor About, Contact, and Services pages

--- optio-blue/src/pages/Services.js
+++ optio-blue/src/pages/Services.js
@@ -3,10 +3,82 @@ import React from 'react';
 
 const Services = () => {
     return (
-        <div>
-            <h1>Our Services</h1>
-            <p>Discover the services we offer to our customers.</p>
-        </div>
+        <>
+            <h1>Services</h1>
+
+            <article>
+                <p>Dry Cleaning</p>
+                <p>
+                    Dry cleaning is a process that utilizes organic solevents other than
+                    water to clean clothes, bedding, upholstery and other types of
+                    fabrics.
+                </p>
+            </article>
+
+            <article>
+                <p>Laundry</p>
+                <p>Laundered clothes and linens. Machine wash and press.</p>
+            </article>
+
+            <article>
+                <p>Alterations</p>
+                <p>
+                    Tailored clothes, suited to an individual. Adjusting clothes to fit a
+                    particular person's unique body shape. Fixed rips and reinforced
+                    stitching to almost all materials.
+                </p>
+            </article>
+
+            <article>
+                <p>Pressing</p>
+                <p>
+                    Removes wrinkles by steaming and ironing garmets. Steaming is a
+                    blowing system where as pressing rubbes an iron across.
+                </p>
+            </article>
+
+            <article>
+                <p>Leather</p>
+                <p>
+                    Natural or synthetic substance that helps loosen and remove accumlated
+                    dirt and grime on leather based material.
+                </p>
+            </article>
+
+            <article>
+                <p>Fur</p>
+                <p>
+                    Process that adds to the longevity of your fur, makes it soft and
+                    supple, conditions the leather, frees the fur of dust particles that
+                    rob your fur of its sheen.
+                </p>
+            </article>
+
+            <article>
+                <p>Bridal</p>
+                <p>
+                    Wedding dress preservation where a series of special cleaning and
+                    packaging techniques are used to ensure bridal gown styles are
+                    retained over time.
+                </p>
+            </article>
+
+            <article>
+                <p>Drapes</p>
+                <p>
+                    Drapery and textile for decorative purposes can be dry cleaned and
+                    pressed ot bring out the original color and liven rooms once again.
+                </p>
+            </article>
+
+            <article>
+                <p>Table & Bedding</p>
+                <p>
+                    Sheets and comforters process in a special cylce that make conforters
+                    look brand new and the sheets crisp and clean.
+                </p>
+            </article>
+        </>
     );
 };
 
