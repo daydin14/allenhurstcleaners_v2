9252adc75c4a16c1c442c3fa6c7eb08355c1a258
David Aydin <david.aydin6@gmail.com>
2024-09-23 12:49:27 -0400

    Refactor Gdetails to use usePlaceDetails hook, updated Gmap with place marker

--- optio-blue/src/components/GoogleMaps/Gdetails.js
+++ optio-blue/src/components/GoogleMaps/Gdetails.js
@@ -2,46 +2,30 @@
 import React from 'react';
 
 // Hooks
+import usePlaceDetails from '../../hooks/usePlaceDetails';
 
 // Components
 import Glink from './Glink';
 
-const Gdetails = () => {
-    // const [placeDetails, setPlaceDetails] = useState(null);
-    // const { googleMaps, error } = useGoogleMapsApi();
+const Gdetails = ({ map, placeId }) => {
+    const { place, status, error } = usePlaceDetails(map, placeId);
 
-    // useEffect(() => {
-    //     if (googleMaps) {
-    //         const service = new googleMaps.places.PlacesService(document.createElement('div'));
-    //         const request = {
-    //             placeId: process.env.REACT_APP_GOOGLE_MAPS_PLACE_ID,
-    //             fields: ['name', 'formatted_address', 'place_id', 'geometry', 'rating', 'formatted_phone_number', 'opening_hours'],
-    //         };
+    if (status !== window.google.maps.places.PlacesServiceStatus.OK) {
+        return <div>Loading place details...</div>;
+    }
 
-    //         service.getDetails(request, (place, status) => {
-    //             if (status === googleMaps.places.PlacesServiceStatus.OK) {
-    //                 setPlaceDetails(place);
-    //             }
-    //         });
-    //     }
-    // }, [googleMaps]);
-
-    // if (error) {
-    //     return <div>Error loading Google Maps API: {error.message}</div>;
-    // }
-
-    // if (!placeDetails) {
-    //     return <div>Loading place details...</div>;
-    // }
+    if (error) {
+        return <div>Error loading place details: {error}</div>;
+    }
 
     return (
         <div className='gdetails'>
             <p>Address: <Glink location="Allenhurst Cleaners" /></p>
-            {/* <h4>{placeDetails.name}</h4>
-            <p>Address: <Glink location={placeDetails.formatted_address} /></p>
-            <p>Rating: {placeDetails.rating}</p>
-            <p>Phone: <a href={`tel:${placeDetails.formatted_phone_number}`}>{placeDetails.formatted_phone_number}</a></p>
-            <p>Email: <a href="mailto:allenhurstcleaners@gmail.com">allenhurstcleaners@gmail.com</a></p> */}
+            <h4>{place.name}</h4>
+            <p>Address: <Glink location={place.formatted_address} /></p>
+            <p>Rating: {place.rating}</p>
+            <p>Phone: <a href={`tel:${place.formatted_phone_number}`}>{place.formatted_phone_number}</a></p>
+            <p>Email: <a href="mailto:allenhurstcleaners@gmail.com">allenhurstcleaners@gmail.com</a></p>
         </div>
     );
 };
