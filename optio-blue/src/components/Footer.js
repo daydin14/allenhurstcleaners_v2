3e773e3de63924948c5246c016c99b34111ec22f
David Aydin <david.aydin6@gmail.com>
2024-09-23 15:40:43 -0400

    Refactor Footer component to use MUI Components and add author link

--- optio-blue/src/components/Footer.js
+++ optio-blue/src/components/Footer.js
@@ -1,18 +1,47 @@
-// Dependencies
-import React from 'react';
+// MUI Components
+import { Box, Typography, Link, Tooltip, useTheme } from '@mui/material';
 
 const Footer = () => {
-    return (
+    const theme = useTheme();
+
+    const companyName = 'Allenhurst Cleaners';
+    const year = new Date().getFullYear();
+    const rights = 'All rights reserved.';
+
+    const author = 'David Aydin';
+    const title = (
         <>
-            <footer className="footer">
-                <div className="footer-text">
-                    <p>Allenhurst Cleaners &copy; {new Date().getFullYear()} All rights reserved.</p>
-                    <span className="tooltip">
-                        By <a href="https://linkedin.com/in/daydin14" target="_blank" rel="noopener noreferrer">David Aydin</a>
-                    </span>
-                </div>
-            </footer>
+            <Link
+                href="https://linkedin.com/in/daydin14"
+                target="_blank"
+                rel="noopener noreferrer"
+                underline="hover"
+                sx={{
+                    color: '#ffffff', // White color for both themes
+                }}
+            >
+                <Typography variant="subtitle2" color="textSecondary" sx={{ color: '#ffffff' }}>
+                    Created by {author}
+                </Typography>
+            </Link>
         </>
+    );
+
+    return (
+        <Box
+            component="footer"
+            sx={{
+                py: 2,
+                textAlign: 'center',
+                backgroundColor: theme.palette.mode === 'light' ? theme.palette.background.default : theme.palette.background.paper,
+            }}
+        >
+            <Tooltip title={title}>
+                <Typography variant="body2" color="textSecondary">
+                    {companyName} &copy; {year} {rights}
+                </Typography>
+            </Tooltip>
+        </Box>
     );
 };
 
