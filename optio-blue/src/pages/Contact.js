308ce6faaf65b3e1649c2c97d9c9208e7faf07fd
David Aydin <david.aydin6@gmail.com>
2024-09-30 16:32:17 -0400

    Refactored Contact Page to use Google Maps Embeded API Map and Custom Toolbar

--- optio-blue/src/pages/Contact.js
+++ optio-blue/src/pages/Contact.js
@@ -2,11 +2,13 @@
 import React, { useState, useRef, useCallback } from 'react';
 
 // MUI Components
-import { Container, Paper, Typography, Box } from '@mui/material';
+import { Box, Container, Paper, Typography } from '@mui/material';
 
 // Components
 import Gmap from '../components/GoogleMaps/Gmap';
 import Gdetails from '../components/GoogleMaps/Gdetails';
+import GmapEmbeded from '../components/GoogleMaps/GmapEmbeded';
+import GmapEmbededToolBar from '../components/GoogleMaps/GmapEmbededToolBar';
 
 const Contact = () => {
     const [map, setMap] = useState(null);
@@ -17,23 +19,49 @@ const Contact = () => {
         setMap(mapInstance);
     }, []);
 
+    const [mode, setMode] = useState('place');
+    const [mapType, setMapType] = useState('roadmap');
+    const [searchQuery, setSearchQuery] = useState('Allenhurst, New Jersey');
+    const [origin, setOrigin] = useState('');
+    const [destination, setDestination] = useState('');
+
+    const toggleMapType = () => {
+        setMapType((prevType) => (prevType === 'roadmap' ? 'satellite' : 'roadmap'));
+    };
+
     return (
         <>
             <Container>
                 <Paper elevation={3} sx={{ padding: 2, marginBottom: 2, textAlign: 'center' }}>
-                    <Typography variant="h4" mt={4}>
+                    <Typography variant="h4">
                         Contact Us!
                     </Typography>
                 </Paper>
                 <Paper elevation={3} sx={{ padding: 2, marginBottom: 2 }}>
-                    {map && (
-                        <Box mt={2}>
-                            <Gdetails map={map} placeId={process.env.REACT_APP_GOOGLE_MAPS_PLACE_ID} />
-                        </Box>
-                    )}
-                    <Box id="map-contact">
-                        <Gmap onLoad={handleMapLoad} />
+                    <Gdetails map={map} placeId={process.env.REACT_APP_GOOGLE_MAPS_PLACE_ID} />
+                    {map && (<></>)} {/* Removes warning for declared but unused 'map' */}
+                    <Box display={'none'}>
+                        <Gmap id="map-contact" onLoad={handleMapLoad} />
                     </Box>
+                </Paper>
+                <Paper elevation={3} sx={{ padding: 2, marginBottom: 2 }}>
+                    <GmapEmbededToolBar
+                        setMode={setMode}
+                        mapType={mapType}
+                        toggleMapType={toggleMapType}
+                        setSearchQuery={setSearchQuery}
+                        setOrigin={setOrigin}
+                        setDestination={setDestination}
+                        mode={mode}
+                    />
+                    <GmapEmbeded
+                        mode={mode}
+                        mapType={mapType}
+                        searchQuery={searchQuery}
+                        origin={origin}
+                        destination={destination}
+                        height={450}
+                    />
                 </Paper>
             </Container>
         </>
