b39090de6d8aad14aaf01dc20348fc2322bd1847
David Aydin <david.aydin6@gmail.com>
2024-12-19 23:24:28 -0500

    puhs

--- optio-blue/src/components/GoogleMaps/Paid-GoogleCloudConsole/Gmap.js
+++ optio-blue/src/components/GoogleMaps/Paid-GoogleCloudConsole/Gmap.js
@@ -12,6 +12,9 @@ import usePlaceDetails from '../../../hooks/usePlaceDetails';
 // Components
 import Gtooltip from './Gtooltip';
 
+// Utils
+import { logEvent } from '../../utils/Ganalytics';
+
 // Map Styles
 const containerStyle = {
     width: '100%',
